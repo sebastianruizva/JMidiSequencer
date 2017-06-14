b21ac1fce77f647d229fc9ff96178e5d08ae595c
Joe Annis <annis.jo@husky.neu.edu>
2017-06-14 15:43:41 -0400

    Changed .ignore file to include .iml files.

--- src/cs3500/music/view/PianoViewPanel.java
+++ src/cs3500/music/view/PianoViewPanel.java
@@ -1,54 +1,69 @@
 package cs3500.music.view;
 
+
+import java.awt.*;
 import java.util.ArrayList;
-import java.util.HashMap;
 
 import javax.swing.*;
 
-import cs3500.music.model.JMidiEvent;
-import cs3500.music.model.JMidiTrack;
-import cs3500.music.model.JVirtualInstrument;
-
-/**
- * Created by joe on 6/13/17.
- */
 public class PianoViewPanel extends JLayeredPane {
-  JMidiTrack track;
-  private HashMap<Integer, HashMap<Integer, JMidiEvent>> grid;
-  private HashMap<String, PianoKey> keys;
+  ArrayList<Integer> selectedPitches;
 
-  public PianoViewPanel(JMidiTrack track) {
-    this.track = track;
-    initPiano();
+  public PianoViewPanel(ArrayList<Integer> selectedPitches) {
+    this.selectedPitches = selectedPitches;
   }
 
-  private void initPiano() {
-    // HashMap<String, PianoKey> keys = new HashMap<>();
-    JVirtualInstrument inst = track.getInstrument();
-    ArrayList<PianoKey> keys = new ArrayList<>();
+  public PianoViewPanel() {
+    this.selectedPitches = new ArrayList<>();
+  }
 
+  @Override
+  public void paint(Graphics g) {
+    paintWhiteKeys(g);
+    paintBlackKeys(g);
 
-    for (int i = 0; i < 87; i++) {
-      String currNote = inst.getNoteRepresentation(i);
-    //  System.out.println(currNote + " " + isNatural(currNote));
-      PianoKey temp;
-
-      if (isNatural(currNote)) {
-        temp = new PianoKey(PianoKey.KeyType.WHITE);
-        temp.setLocation(i * 10 + (i * 10 % 20), 0);
-        add(temp, temp.getDepth(), -1);
-
-      } else {
-        System.out.println(currNote + " " + isNatural(currNote) + " " + i+ " " +i * 10);
-        temp = new PianoKey(PianoKey.KeyType.BLACK);
-        temp.setLocation(i * 10 - (i * 10 % 20), 0);
-        add(temp, temp.getDepth(), -1);
-
-      }
+    for (int i : selectedPitches) {
+      fillRectangle(g, i);
     }
   }
 
-  private boolean isNatural(String name) {
-    return !name.contains("#");
+  private void paintWhiteKeys(Graphics g) {
+    int size = DrawValues.RECTANGLE_H;
+
+    for (int i = 0; i < 70; i++) {
+      g.setColor(Color.WHITE);
+      g.drawRect(i * size, 0, size, 350);
+      g.fillRect(i * size, 0, size, 350);
+      g.setColor(Color.BLACK);
+      g.drawRect(i * size, 0, size, 350);
+
+      System.out.println("X: " + i * size);
+    }
+  }
+
+  private void paintBlackKeys(Graphics g) {
+    int skip = 2;
+    int oddCount = 0;
+    int size = DrawValues.RECTANGLE_H;
+
+    for (int i = 0; i < 70; i++) {
+
+      if (i == skip) {
+        if (oddCount % 2 == 0) {
+          skip += 4;
+        } else {
+          skip += 3;
+        }
+        oddCount++;
+        continue;
+      }
+      g.setColor(Color.BLACK);
+      g.drawRect(i * size + size - 4, 0, size / 2, 175);
+      g.fillRect(i * size + size - 4, 0, size / 2, 175);
+    }
+  }
+
+  public void fillRectangle(Graphics g, int pitch) {
+
   }
 }
